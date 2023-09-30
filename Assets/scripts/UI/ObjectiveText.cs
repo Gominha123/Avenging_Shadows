5f51a19c20795bff6f451f563dde44615e28b322
LS-KDE45 <luisqueiros21@gmail.com>
2023-09-30 17:44:58 +0100

    Objective UI
    UI do objetivo atual

---
+++ Assets/scripts/UI/ObjectiveText.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ObjectiveText : MonoBehaviour
+{
+    [SerializeField] private GameObject uiPanel;
+    [SerializeField] private TextMeshProUGUI objectiveTextTMP;
+
+    public bool isDisplayed = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUp("Find the Map");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetUp(string objectiveText)
+    {
+        objectiveTextTMP.text = objectiveText;
+        uiPanel.SetActive(true);
+        isDisplayed = true;
+    }
+
+    public void Close()
+    {
+        uiPanel.SetActive(false);
+        isDisplayed = false;
+    }
+}

77d4931a7a365e7903dbbbc50801e37a06943873
Gominha123 <a21666@alunos.ipca.pt>
2023-11-20 10:55:22 +0000

    build fix

--- Assets/scripts/enemy/AISimples.cs
+++ Assets/scripts/enemy/AISimples.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using Unity.VisualScripting;
-using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.AI;
 using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
